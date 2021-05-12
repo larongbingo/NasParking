3f3582447b67116f09d140faa3089468a4884eb4
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-05-13 01:07:12 +0800

    refactor: remove unnecessary condition

--- src/Middlewares/GeneralErrorHandling.ts
+++ src/Middlewares/GeneralErrorHandling.ts
@@ -3,10 +3,5 @@ import { ErrorRequestHandler } from "express";
 export const GeneralErrorHandlingMiddleware: ErrorRequestHandler = (err, req, res, next) => {
     res.status(err.status || 500);
 
-    if(process.env.NODE_ENV === "development") {
-        res.json({message: err.message});    
-    }
-    else {
-        res.json({message: "An error occured"});
-    }
+    res.json({message: err.message || "An error occurred"});   
 };
