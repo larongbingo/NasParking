a0b0c5e3230fad018b9e07532f1fbb233efd2d28
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-05-12 16:04:18 +0800

    chore: Rename express app

--- src/App.ts
+++ src/App.ts
@@ -4,3 +4,8 @@ import { Routes } from "./Routes";
 export const App = Express();
 
 App.use(Routes);
+
+App.use((req, res) => {
+    res.json({message: "Route does not exist"});
+    res.status(404);
+});
