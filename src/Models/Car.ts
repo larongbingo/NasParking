6adab340fd41226c72e594c73f4d7b17b63c97b5
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2021-05-12 12:22:41 +0800

    feat: add parkedSince

--- src/Models/Car.ts
+++ src/Models/Car.ts
@@ -1,8 +1,10 @@
 
 export class Car {
     public constructor(plateNumber: string) {
-        this.PlateNumber =  plateNumber;
+        this.PlateNumber = plateNumber;
+        this.ParkedSince = new Date(); 
     }
 
     public readonly PlateNumber: string;
+    public readonly ParkedSince: Date;
 }
